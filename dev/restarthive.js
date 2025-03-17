de17c8496bbde716aef0354d484a6771a0b3606d
spacevoodoo-bitburner <achristiananarchist@gmail.com>
2025-03-17 18:57:38 -0500

    Create restarthive.js
    restart hives when they are below a certain level of server utilization

---
+++ dev/restarthive.js
@@ -0,0 +1,7 @@
+export async function main(ns) {
+  let host = ns.getHostname();
+  ns.killall(host, true);
+  ns.exec("/dev/queen.js", host, 1, 1000);
+  ns.sleep(1000);
+  ns.exec("/dev/hive.js", host);
+}
