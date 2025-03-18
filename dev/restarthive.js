1ecb71215a8aa8e8813a0f0189c81ca11debe701
spacevoodoo-bitburner <achristiananarchist@gmail.com>
2025-03-17 19:08:56 -0500

    Update restarthive.js

--- dev/restarthive.js
+++ dev/restarthive.js
@@ -2,6 +2,6 @@ export async function main(ns) {
   let host = ns.getHostname();
   ns.killall(host, true);
   ns.exec("/dev/queen.js", host, 1, 1000);
-  ns.sleep(1000);
+  await ns.sleep(1000);
   ns.exec("/dev/hive.js", host);
 }
