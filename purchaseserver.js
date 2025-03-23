16310403f21355f5ef559ee6a10d7b5f2748b8ec
spacevoodoo-bitburner <achristiananarchist@gmail.com>
2025-03-23 16:20:56 -0500

    Update purchaseserver.js
    purchase and upgrade server rolled into one thing

--- purchaseserver.js
+++ purchaseserver.js
@@ -1,12 +1,32 @@
 /** @param {NS} ns */
 export async function main(ns) {
   const ram = 8;
-  let i = 0;
+  let i = ns.getPurchasedServers().length;
   while (i < ns.getPurchasedServerLimit()) {
     if (ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram)) {
-      let hostname = ns.purchaseServer("pserv-" + i, ram);
+      ns.purchaseServer("pserv-" + i, ram);
+      ns.scp("/dev/hive.js", "pserv-" + i);
+      ns.scp("/dev/worker.js", "pserv-" + i);
+      ns.scp("/dev/scanner.js", "pserv-" + i);
+      ns.scp("/dev/queen.js", "pserv-" + i);
+      ns.scp("/dev/restarthive.js", "pserv-" + i);
+      ns.exec("/dev/queen.js", "pserv-" + i, 1, 1000);
+      await ns.sleep(100);
+      ns.exec("/dev/hive.js", "pserv-" + i);
       ++i;
     }
     await ns.sleep(1000);
+      
+  }
+  while (i == ns.getPurchasedServerLimit()){
+    const servers = ns.getPurchasedServers()
+    for (let j = 0; j < servers.length; j++){
+      let ram = ns.getServerMaxRam(servers[j]) * 2;
+      ns.getPurchasedServerUpgradeCost(servers[j], ram)
+      if (ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram) && ram < ns.args[0]){
+        ns.upgradePurchasedServer(servers[j], ram);
+      }
+    }
+    await ns.sleep(10000);
   }
 }
